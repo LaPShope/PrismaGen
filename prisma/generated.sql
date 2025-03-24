0a1e7f6578e15f21714e9b808ae8fb44b578f0b4
alexNg <ntriet0612@gmail.com>
2025-03-25 00:42:02 +0700

    remap

--- prisma/generated.sql
+++ prisma/generated.sql
@@ -1,26 +1,19 @@
 -- CreateTable
-CREATE TABLE `Account` (
+CREATE TABLE `account` (
     `id` BINARY(16) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `password` VARCHAR(191) NOT NULL,
-    `role` VARCHAR(191) NOT NULL,
+    `role` ENUM('Customer', 'Admin') NOT NULL,
 
-    UNIQUE INDEX `Account_email_key`(`email`),
+    UNIQUE INDEX `account_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Role` (
-    `name` VARCHAR(191) NOT NULL DEFAULT 'customer',
-
-    PRIMARY KEY (`name`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Customer` (
+CREATE TABLE `customer` (
     `account_id` BINARY(16) NOT NULL,
-    `gender` VARCHAR(191) NOT NULL,
+    `gender` ENUM('Male', 'Female', 'Other') NOT NULL,
     `born_date` DATETIME(3) NOT NULL,
     `phone` VARCHAR(191) NOT NULL,
     `avatar` VARCHAR(191) NULL,
@@ -29,36 +22,37 @@ CREATE TABLE `Customer` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Admin` (
+CREATE TABLE `admin` (
     `account_id` BINARY(16) NOT NULL,
 
     PRIMARY KEY (`account_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `LaptopModel` (
+CREATE TABLE `laptop_model` (
     `id` BINARY(16) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `brand` VARCHAR(191) NOT NULL,
     `cpu` VARCHAR(191) NOT NULL,
     `ram` VARCHAR(191) NOT NULL,
+    `gpu` VARCHAR(191) NOT NULL,
     `storage` VARCHAR(191) NOT NULL,
     `display` VARCHAR(191) NOT NULL,
     `color` ENUM('Red', 'Blue', 'Green', 'Black', 'White', 'Silver', 'Gold') NOT NULL,
     `price` DECIMAL(10, 2) NOT NULL,
     `description` VARCHAR(191) NULL,
 
-    INDEX `LaptopModel_cpu_idx`(`cpu`),
-    INDEX `LaptopModel_ram_idx`(`ram`),
-    INDEX `LaptopModel_brand_idx`(`brand`),
-    INDEX `LaptopModel_cpu_ram_idx`(`cpu`, `ram`),
-    INDEX `LaptopModel_cpu_ram_brand_idx`(`cpu`, `ram`, `brand`),
-    INDEX `LaptopModel_cpu_ram_brand_price_idx`(`cpu`, `ram`, `brand`, `price`),
+    INDEX `laptop_model_cpu_idx`(`cpu`),
+    INDEX `laptop_model_ram_idx`(`ram`),
+    INDEX `laptop_model_brand_idx`(`brand`),
+    INDEX `laptop_model_cpu_ram_idx`(`cpu`, `ram`),
+    INDEX `laptop_model_cpu_ram_brand_idx`(`cpu`, `ram`, `brand`),
+    INDEX `laptop_model_cpu_ram_brand_price_idx`(`cpu`, `ram`, `brand`, `price`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `LaptopOnSale` (
+CREATE TABLE `laptop_on_sale` (
     `laptop_model_id` BINARY(16) NOT NULL,
     `sale_id` BINARY(16) NOT NULL,
 
@@ -66,7 +60,7 @@ CREATE TABLE `LaptopOnSale` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Sale` (
+CREATE TABLE `sale` (
     `id` BINARY(16) NOT NULL,
     `event_description` VARCHAR(191) NULL,
     `start_at` DATETIME(3) NOT NULL,
@@ -77,45 +71,47 @@ CREATE TABLE `Sale` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Laptop` (
+CREATE TABLE `laptop` (
     `mac_id` BINARY(16) NOT NULL,
     `MFG` DATETIME(3) NOT NULL,
     `model_id` BINARY(16) NOT NULL,
     `status` ENUM('Available', 'SoldOut', 'Maintenance') NOT NULL DEFAULT 'Available',
 
-    INDEX `Laptop_status_idx`(`status`),
-    INDEX `Laptop_model_id_idx`(`model_id`),
+    INDEX `laptop_status_idx`(`status`),
+    INDEX `laptop_model_id_idx`(`model_id`),
     PRIMARY KEY (`mac_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Order` (
+CREATE TABLE `order` (
     `id` BINARY(16) NOT NULL,
     `customer_id` BINARY(16) NOT NULL,
     `status` ENUM('Pending', 'Shipped', 'Delivered', 'Cancelled') NOT NULL DEFAULT 'Pending',
     `address` VARCHAR(191) NOT NULL,
     `date_create` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `delivery_cost` DECIMAL(10, 2) NOT NULL,
+    `final_price` DECIMAL(10, 2) NOT NULL,
 
-    INDEX `Order_customer_id_idx`(`customer_id`),
-    INDEX `Order_status_idx`(`status`),
+    INDEX `order_customer_id_idx`(`customer_id`),
+    INDEX `order_status_idx`(`status`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `OrderDetail` (
+CREATE TABLE `order_detail` (
     `id` BINARY(16) NOT NULL,
     `order_id` BINARY(16) NOT NULL,
     `laptop_model_id` BINARY(16) NOT NULL,
     `quantity` INTEGER NOT NULL,
     `price` DECIMAL(10, 2) NOT NULL,
 
-    INDEX `OrderDetail_order_id_idx`(`order_id`),
-    INDEX `OrderDetail_laptop_model_id_idx`(`laptop_model_id`),
+    INDEX `order_detail_order_id_idx`(`order_id`),
+    INDEX `order_detail_laptop_model_id_idx`(`laptop_model_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Payment` (
+CREATE TABLE `payment` (
     `id` BINARY(16) NOT NULL,
     `customer_id` BINARY(16) NOT NULL,
     `order_id` BINARY(16) NOT NULL,
@@ -123,27 +119,27 @@ CREATE TABLE `Payment` (
     `status` ENUM('Pending', 'Success', 'Failed') NOT NULL,
     `payment_method_id` BINARY(16) NULL,
 
-    UNIQUE INDEX `Payment_order_id_key`(`order_id`),
-    INDEX `Payment_customer_id_idx`(`customer_id`),
-    INDEX `Payment_order_id_idx`(`order_id`),
-    INDEX `Payment_status_idx`(`status`),
-    INDEX `Payment_type_idx`(`type`),
-    INDEX `Payment_payment_method_id_idx`(`payment_method_id`),
+    UNIQUE INDEX `payment_order_id_key`(`order_id`),
+    INDEX `payment_customer_id_idx`(`customer_id`),
+    INDEX `payment_order_id_idx`(`order_id`),
+    INDEX `payment_status_idx`(`status`),
+    INDEX `payment_type_idx`(`type`),
+    INDEX `payment_payment_method_id_idx`(`payment_method_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `PaymentMethod` (
+CREATE TABLE `payment_method` (
     `id` BINARY(16) NOT NULL,
     `data` JSON NOT NULL,
     `type` ENUM('VNPay', 'Momo', 'ZaloPay', 'CashOnDelivery') NOT NULL,
 
-    INDEX `PaymentMethod_type_idx`(`type`),
+    INDEX `payment_method_type_idx`(`type`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `LaptopOnImage` (
+CREATE TABLE `laptop_on_image` (
     `laptop_model_id` BINARY(16) NOT NULL,
     `image_id` BINARY(16) NOT NULL,
 
@@ -151,7 +147,7 @@ CREATE TABLE `LaptopOnImage` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Image` (
+CREATE TABLE `image` (
     `id` BINARY(16) NOT NULL,
     `image_url` VARCHAR(191) NOT NULL,
 
@@ -159,21 +155,21 @@ CREATE TABLE `Image` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Comment` (
+CREATE TABLE `comment` (
     `id` BINARY(16) NOT NULL,
     `laptop_model_id` BINARY(16) NOT NULL,
     `account_id` BINARY(16) NOT NULL,
     `parent_id` BINARY(16) NULL,
     `body` VARCHAR(191) NOT NULL,
 
-    INDEX `Comment_laptop_model_id_idx`(`laptop_model_id`),
-    INDEX `Comment_account_id_idx`(`account_id`),
-    INDEX `Comment_parent_id_idx`(`parent_id`),
+    INDEX `comment_laptop_model_id_idx`(`laptop_model_id`),
+    INDEX `comment_account_id_idx`(`account_id`),
+    INDEX `comment_parent_id_idx`(`parent_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Chat` (
+CREATE TABLE `chat` (
     `id` BINARY(16) NOT NULL,
     `sender_id` BINARY(16) NOT NULL,
     `receiver_id` BINARY(16) NOT NULL,
@@ -184,7 +180,7 @@ CREATE TABLE `Chat` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Address` (
+CREATE TABLE `address` (
     `id` BINARY(16) NOT NULL,
     `customer_id` BINARY(16) NOT NULL,
     `city` VARCHAR(191) NOT NULL,
@@ -193,22 +189,21 @@ CREATE TABLE `Address` (
     `street` VARCHAR(191) NOT NULL,
     `phone` VARCHAR(191) NOT NULL,
 
-    INDEX `Address_customer_id_idx`(`customer_id`),
+    INDEX `address_customer_id_idx`(`customer_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Cart` (
+CREATE TABLE `cart` (
     `id` BINARY(16) NOT NULL,
     `customer_id` BINARY(16) NOT NULL,
-    `quantity` INTEGER NOT NULL,
 
-    INDEX `Cart_customer_id_idx`(`customer_id`),
+    INDEX `cart_customer_id_idx`(`customer_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `LaptopOnCart` (
+CREATE TABLE `laptop_on_cart` (
     `cart_id` BINARY(16) NOT NULL,
     `laptop_model_id` BINARY(16) NOT NULL,
     `quantity` INTEGER NOT NULL,
@@ -217,71 +212,68 @@ CREATE TABLE `LaptopOnCart` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Account` ADD CONSTRAINT `Account_role_fkey` FOREIGN KEY (`role`) REFERENCES `Role`(`name`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `customer` ADD CONSTRAINT `customer_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Customer` ADD CONSTRAINT `Customer_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `admin` ADD CONSTRAINT `admin_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Admin` ADD CONSTRAINT `Admin_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `laptop_on_sale` ADD CONSTRAINT `laptop_on_sale_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `laptop_model`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `LaptopOnSale` ADD CONSTRAINT `LaptopOnSale_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `LaptopModel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `laptop_on_sale` ADD CONSTRAINT `laptop_on_sale_sale_id_fkey` FOREIGN KEY (`sale_id`) REFERENCES `sale`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `LaptopOnSale` ADD CONSTRAINT `LaptopOnSale_sale_id_fkey` FOREIGN KEY (`sale_id`) REFERENCES `Sale`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `laptop` ADD CONSTRAINT `laptop_model_id_fkey` FOREIGN KEY (`model_id`) REFERENCES `laptop_model`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Laptop` ADD CONSTRAINT `Laptop_model_id_fkey` FOREIGN KEY (`model_id`) REFERENCES `LaptopModel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `order` ADD CONSTRAINT `order_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Order` ADD CONSTRAINT `Order_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `order_detail` ADD CONSTRAINT `order_detail_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `OrderDetail` ADD CONSTRAINT `OrderDetail_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `order_detail` ADD CONSTRAINT `order_detail_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `laptop_model`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `OrderDetail` ADD CONSTRAINT `OrderDetail_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `LaptopModel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `payment` ADD CONSTRAINT `payment_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Payment` ADD CONSTRAINT `Payment_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `payment` ADD CONSTRAINT `payment_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Payment` ADD CONSTRAINT `Payment_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `payment` ADD CONSTRAINT `payment_payment_method_id_fkey` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_method`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Payment` ADD CONSTRAINT `Payment_payment_method_id_fkey` FOREIGN KEY (`payment_method_id`) REFERENCES `PaymentMethod`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `laptop_on_image` ADD CONSTRAINT `laptop_on_image_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `laptop_model`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `LaptopOnImage` ADD CONSTRAINT `LaptopOnImage_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `LaptopModel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `laptop_on_image` ADD CONSTRAINT `laptop_on_image_image_id_fkey` FOREIGN KEY (`image_id`) REFERENCES `image`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `LaptopOnImage` ADD CONSTRAINT `LaptopOnImage_image_id_fkey` FOREIGN KEY (`image_id`) REFERENCES `Image`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `comment` ADD CONSTRAINT `comment_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `laptop_model`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Comment` ADD CONSTRAINT `Comment_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `LaptopModel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `comment` ADD CONSTRAINT `comment_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Comment` ADD CONSTRAINT `Comment_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `comment` ADD CONSTRAINT `comment_parent_id_fkey` FOREIGN KEY (`parent_id`) REFERENCES `comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Comment` ADD CONSTRAINT `Comment_parent_id_fkey` FOREIGN KEY (`parent_id`) REFERENCES `Comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `chat` ADD CONSTRAINT `chat_sender_id_fkey` FOREIGN KEY (`sender_id`) REFERENCES `account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Chat` ADD CONSTRAINT `Chat_sender_id_fkey` FOREIGN KEY (`sender_id`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `chat` ADD CONSTRAINT `chat_receiver_id_fkey` FOREIGN KEY (`receiver_id`) REFERENCES `account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Chat` ADD CONSTRAINT `Chat_receiver_id_fkey` FOREIGN KEY (`receiver_id`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `address` ADD CONSTRAINT `address_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Address` ADD CONSTRAINT `Address_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `cart` ADD CONSTRAINT `cart_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Cart` ADD CONSTRAINT `Cart_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`account_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `laptop_on_cart` ADD CONSTRAINT `laptop_on_cart_cart_id_fkey` FOREIGN KEY (`cart_id`) REFERENCES `cart`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `LaptopOnCart` ADD CONSTRAINT `LaptopOnCart_cart_id_fkey` FOREIGN KEY (`cart_id`) REFERENCES `Cart`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `LaptopOnCart` ADD CONSTRAINT `LaptopOnCart_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `LaptopModel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `laptop_on_cart` ADD CONSTRAINT `laptop_on_cart_laptop_model_id_fkey` FOREIGN KEY (`laptop_model_id`) REFERENCES `laptop_model`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
